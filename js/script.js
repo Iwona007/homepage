c74ee0a85392ae8ab653567994f4d03328ade319
Iwona007 <iwonka.adamkiewicz@gmail.com>
2021-05-03 01:09:19 +0200

    add function to JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
-console.log("Hello my friend :)")
+console.log("Hello my friend")
+
+let przyciskHide = document.querySelector(".przyciskUkryj")
+let przyciskShow = document.querySelector(".przyciskShow")
+let przycisktext = document.querySelector(".textShow")
+
+let slonceAkcji = document.querySelector(".slonce");
+let target = document.querySelector(".slonce")
+
+// const newElement = slonceAkcji.cloneNode()
+// console.log(newElement)
+
+// const clone2 = slonceAkcji.cloneNode(true)
+// console.log(clone2)
+
+
+przyciskShow.addEventListener("click", e => {
+    let slonceAkcji = document.querySelector(".slonce");
+    let clone = slonceAkcji.cloneNode(true)
+    target.appendChild(clone)
+
+});
+
+przyciskHide.addEventListener("click", () => {
+    slonceAkcji.remove()
+    console.log("Przycisk jest usunięty")
+});
+
+przycisktext.addEventListener("click", e => {
+    przycisktext.innerText = "Nowy text"
+
+    console.log("tekst który się wyświetla w konsoli developera")
+})
+
+
+
+
+
+
+
+
+
